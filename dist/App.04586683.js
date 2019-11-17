ebb2aae14b660b4cabde88d585b05b1972b6d639
lanoid <916179+lanoid@users.noreply.github.com>
2019-11-17 11:46:12 +0000

    audit fix

--- dist/App.04586683.js
+++ dist/App.04586683.js
@@ -72224,7 +72224,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "49493" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "60338" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
